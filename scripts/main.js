5af7ff9d3f4eec724c56cb5e0945c044c849ee2a
Dariia <91015233+Dariia-Asmolova@users.noreply.github.com>
2023-06-20 00:22:14 +0300

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,30 @@ function openNav() {
 }
 
 burger.addEventListener("click", openNav);
+
+const modalPolicy = document.getElementById("policy");
+const openPolicy = document.getElementById("openPolicy");
+const closePolicy = document.getElementById("closePolicy");
+const modalTerms = document.getElementById("terms");
+const openTerms = document.getElementById("openTerms");
+const closeTerms = document.getElementById("closeTerms");
+openTerms.onclick = () => {
+  modalTerms.style.display = "flex";
+};
+closeTerms.onclick = () => {
+  modalTerms.style.display = "none";
+};
+openPolicy.onclick = () => {
+  modalPolicy.style.display = "flex";
+};
+closePolicy.onclick = () => {
+  modalPolicy.style.display = "none";
+};
+window.onclick = (event) => {
+  if (event.target == modalTerms) {
+    modalTerms.style.display = "none";
+  }
+  if (event.target == modalPolicy) {
+    modalPolicy.style.display = "none";
+  }
+};
